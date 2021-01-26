a03a36239b11116eef48509f5a6017db452141d9
Rodolfo.Cotemig <71801111@aluno.faculdadecotemig.br>
2021-01-25 23:34:22 -0300

    novo backgroundimage

--- pages/index.js
+++ pages/index.js
@@ -28,8 +28,8 @@ import QuizLogo from '../src/components/QuizLogo'
 export const QuizContainer = styled.div`
   width: 100%;
   max-width: 350px;
-  padding-top: 300px;
-  margin: auto 69%;
+  padding-top: 45px;
+  margin: auto 10%;
   @media screen and (max-width: 500px) {
     margin: auto;
     padding: 15px;
